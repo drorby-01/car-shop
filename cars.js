38429d2d1d71f4a159a01783322a1ca167fe0326
drorby-01 <drorby23@gmail.com>
2020-07-02 15:42:53 +0300

    show table header sunroof by checkbox

--- cars.js
+++ cars.js
@@ -7,9 +7,43 @@ const displayFunctions = {
     "cards": getCardItem,
     "list": getListItem,
     "table": getRowItem,
+    "tableHeader": getRowHeaderItem
 };
-console.log(displayFunctions)
 
+const headers = [
+    [
+    {
+        value: "lp",
+        label: "LP",
+        isVisible: true,
+    },
+    {
+        value: "color",
+        label: "Color",
+        isVisible: true,
+    },
+    {
+        value: "type",
+        label: "Type",
+        isVisible: true
+    },
+    {
+        value: "doors",
+        label: "Doors",
+        isVisible: true
+    },
+    {
+        value: "isSunRoof",
+        label: "Sun Roof",
+        isVisible: false
+    },
+    {
+        value: "isAWD",
+        label: "4 X 4",
+        isVisible: false
+    }
+]
+]
 
 function generateCars(numberOfCars, isArray) { //return array with Cars ( each car is an object in JS)
     if (typeof numberOfCars !== 'number') return;
@@ -30,7 +64,8 @@ function generateSingleCar(index) {
         color: _generateColor(),
         type: _generateType(),
         doors: _generateDoors(),
-        isSunRoof: _isSunRoof(index)
+        isSunRoof: _isSunRoof(index),
+        isAWD: _isAWD(index)
     };
 
 
@@ -49,6 +84,9 @@ function generateSingleCar(index) {
     function _generateType() {
         return types[Math.floor(Math.random() * types.length)];
     }
+    function _isAWD(index) {
+        return index % 2 === 0 ? true : false
+    }
 
 }
 
@@ -61,36 +99,91 @@ function generateSingleCar(index) {
 
 
 (function () {
-    const cars = generateCars(100, true)
-    DOM.listData = document.getElementById("data");
-    DOM.cardsData = document.getElementById("data-cards");
-    draw(cars, DOM.listData, "list");
+    const cars = generateCars(100, true)//return 100 cars array 
+    DOM.listData = document.getElementById("data");//point to the list container
+    DOM.cardsData = document.getElementById("data-cards");//point to the cards container
+    DOM.tableData = document.getElementById("table-data");
+    DOM.tableHead = document.getElementById("table-head");
+    DOM.whatToDraw = "list"//give me feedback what the last container i draw on 
+
+    draw(cars, DOM.listData, DOM.whatToDraw);
 
     const listViewButton = document.getElementById("listView");
     const cardViewButton = document.getElementById("cardView");
+    const tableViewButton = document.getElementById("tableView");
+    const searchOperation = document.getElementById("searchOperation");
+
+    const checkBoxIsSunRoof = document.getElementById("checkBoxIsSunRoof");
+    console.log(checkBoxIsSunRoof)
+
+    checkBoxIsSunRoof.addEventListener("change",(e)=>{
+        const showSunRoof = headers[0].find((element)=> element.value === "isSunRoof")
+        
+        if(e.target.checked){
+            showSunRoof.isVisible = true
+
+        }
+        else {showSunRoof.isVisible = false}
+        }
+    )
+
     listViewButton.addEventListener("click", function () {
+        DOM.whatToDraw = "list";//update the dom pointer container 
         draw(cars, DOM.listData, "list")
     })
     cardViewButton.addEventListener("click", function () {
+        DOM.whatToDraw = "cards"
         draw(cars, DOM.cardsData, "cards")
+    })
+    tableViewButton.addEventListener("click", function () {
+        DOM.whatToDraw = "table"
+        draw(cars, DOM.tableData, "table")
+        draw(headers, DOM.tableHead, "tableHeader", false)
+    })
+
+    searchOperation.addEventListener("click", function () {
+        const value = document.getElementById("searchValue").value;
+        if (!value) return;
+        const result = cars.filter(car => { return car.type.toLowerCase() === value.toLowerCase() })
+        if (DOM.whatToDraw === "table") {
+            draw(result, DOM.tableData, "table")
+            draw(headers, DOM.tableHead, "tableHeader", false)
+        }
+        if (DOM.whatToDraw === "cards") {
+            draw(result, DOM.cardsData, "cards")
+        }
+        if (DOM.whatToDraw === "list") {
+            draw(result, DOM.listData, "list")
+        }
+
     })
 }())
 
 
-function draw(data, domContainer, displayType) {
-    clearDOM()
+function draw(data, domContainer, displayType, clear = true) {
+    if (clear) clearDOM()
     if (!Array.isArray(data)) return;
     if (typeof domContainer !== 'object') return;
     const displayFunction = displayFunctions[displayType]
     if (typeof displayFunction !== 'function') return;
-    data.forEach(car => {
-        domContainer.append(displayFunction(car))
+    data.forEach(item => {
+        const result = displayFunction(item)
+        domContainer.append(result)
     });
 }
 
 function clearDOM() {
-    DOM.listData.innerHTML = "";
-    DOM.cardsData.innerHTML = "";
+    // DOM.listData.innerHTML = "";
+    // DOM.cardsData.innerHTML = "";
+    // DOM.tableData.innerHTML = "";
+
+    // this is more dynamic
+
+    Object.keys(DOM).forEach((keyInDom) => {
+        if (typeof DOM[keyInDom] !== "object") return;
+        DOM[keyInDom].innerHTML = "";
+    })
+
 }
 function getListItem(carData) {
     const listItem = document.createElement("li");
@@ -105,7 +198,66 @@ function getCardItem(carData) {
     card.style.height = "50px";
     card.style.width = "300px";
     card.style.display = "inline-block";
-    card.innerText = `car lp: ${carData.lp}, car color: ${carData.color}`;
+    card.innerText = `car lp: ${carData.lp}, car color: ${carData.color} , car type: ${carData.type}`;
     return card;
 }
-function getRowItem() { }
+
+function getRowHeaderItem(headers) {
+    const ths = headers.filter((header) => { return header.isVisible }).map(header => {
+        const { label, isVisible } = header;
+        if (isVisible) return _getTH(label)
+    })
+    const tr = _getTR();
+    tr.append(...ths);
+    return tr;
+    function _getTR() {
+        return document.createElement("TR");
+    }
+
+    function _getTH(value) {
+        const th = document.createElement("TH");
+        th.style.color = "red";
+        th.innerText = value;
+        return th;
+    }
+}
+function getRowItem(carData) {
+    // const lp = carData.lp;
+    // const color = carData.color;
+    // const type = carData.type;
+    // const doors = carData.doors;
+    const { lp, type, doors, color } = carData; // destructuring es6 
+    // return getRowItem;
+
+    const tr = _getTR();
+    const firstRowFromHeaders = headers[0];
+    const visibleHeaders = firstRowFromHeaders.filter((header) => { return header.isVisible });
+    const tds = visibleHeaders.map((header) => {
+        const { value } = header;
+        const currentValue = carData[value];
+        return _getTD(currentValue)
+    })
+
+    // const tdLP = _getTD(lp);
+    // const tdColor = _getTD(color);
+    // const tdType = _getTD(type);
+    // const tdDoors = _getTD(doors);
+    tr.append(...tds)
+    return tr;
+
+    function _getTR() {
+        return document.createElement("tr");
+    }
+
+    function _getTD(value) {
+        const allowedTypes = ["string", "number", "boolean"];
+        const theType = typeof value;
+        // if (!allowedTypes.includes(theType)) return;
+        let currentValue = !allowedTypes.includes(theType) ? "-" : value
+        // if (theType !== "string" && theType !== "number") return;
+        const td = document.createElement("td");
+        td.innerText = currentValue;
+        return td;
+    }
+}
+
